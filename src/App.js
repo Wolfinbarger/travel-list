9bebbfb3f326146c5f460887b7e02895c84db65a
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-08-07 20:50:24 -0500

    prevent page refresh

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -30,6 +34,8 @@ function Form() {
           </option>
         ))}
       </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </form>
   );
 }
